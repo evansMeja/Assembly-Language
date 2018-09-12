ce884f3592ef2390c6bb5992391c303e205049c4
evanslee <35095785+evansMeja@users.noreply.github.com>
2018-09-12 04:54:47 +0300

    Add files via upload

---
+++ stack.asm
@@ -0,0 +1,35 @@
+section .data
+   	digit db 0,10
+
+section .text
+	global _start
+
+_start:
+	push 8
+	push 4
+	push 1
+	
+	pop rax
+	call _printRAXDigit
+
+	pop rax
+        call _printRAXDigit
+
+	pop rax
+        call _printRAXDigit
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_printRAXDigit:
+	add rax, 48
+	mov [digit], al
+	mov rax, 1
+        mov rdi, 1
+        mov rsi, digit
+        mov rdx, 2
+        syscall
+	ret
+
+
